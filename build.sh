c2f71c5c13b7884c1759395c6221ee6e8dbd30ad
Fabio <f.pierri@synclab.net>
2020-09-15 21:47:13 +0200

    added image file to build and docker compose script

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -xev
 echo "Script name: build odoo image"
 echo "*****************************"
 
-docker build --force-rm	--tag odoo12-20200915:latest .
+docker build --force-rm	--tag ghcr.io/manprint/odoo12-20200915:latest .
 
